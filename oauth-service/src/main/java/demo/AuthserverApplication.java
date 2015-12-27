ae4d2091f84c5c5a8a334833a528fe1bdaf92a2b
vankor <vankor@bigmir.net>
2015-12-27 21:27:44 +0200

    last changes

---
+++ oauth-service/src/main/java/demo/AuthserverApplication.java
@@ -0,0 +1,79 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Arrays;
+
+@SpringBootApplication
+@RestController
+@EnableResourceServer
+public class AuthserverApplication extends WebMvcConfigurerAdapter {
+
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+        }
+
+
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("acme")
+                    .secret("acmesecret").authorizedGrantTypes("password", "authorization_code", "refresh_token")
+                    .authorities("ROLE_USER").scopes("write");
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthserverApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+              return user;
+    }
+
+
+
+
+    }
+
+
---
+++ resource-service/src/test/groovy/demo/ApplicationTests.groovy
@@ -0,0 +1,34 @@
+package demo
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.IntegrationTest
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.boot.test.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.web.client.RestTemplate
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringApplicationConfiguration(classes = ResourceApplication)
+@WebAppConfiguration
+@IntegrationTest('server.port:0')
+public class ApplicationTests {
+
+	@Value('${local.server.port}')
+	private int port;
+
+	private RestTemplate template = new TestRestTemplate();
+
+	@Test
+	public void resourceLoads() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:${port}/", String.class)
+		Assert.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode())
+		String auth = response.getHeaders().getFirst("WWW-Authenticate")
+		Assert.assertTrue("Wrong location: " + auth , auth.startsWith("Bearer"))
+	}
+}
